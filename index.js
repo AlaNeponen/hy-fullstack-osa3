473076fcf2cbc6ed89f47f725adbf2dc012bfa4a
Teemu Närhi <t.a.narhi@gmail.com>
2021-01-05 01:44:21 +0200

    3.15 ja 16

--- index.js
+++ index.js
@@ -4,22 +4,27 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
 require('dotenv').config()
 const Person = require('./models/person')
 
 app.get(`/api/persons`, (request, response) => {
     Person.find({}).then(people => {
-      response.json(people)
+      response.json(people.map(person => person.toJSON()))
     })
 })
-app.get(`/api/persons/:id`, (request, response) => {
+app.get(`/api/persons/:id`, (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-      response.json(person)
+      if (person) {
+        response.json(person.toJSON())
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))  
 })
 app.get(`/info`, (request, response) => {
     const now = new Date().toUTCString()
@@ -28,9 +33,10 @@ app.get(`/info`, (request, response) => {
     
 })
 app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(
+  Person.findByIdAndDelete(request.params.id).then(result => {
     response.status(204).end()
-  )
+  })
+  .catch(error => next(error))
 })
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -54,6 +60,18 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unkown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  next(error)
+}
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
